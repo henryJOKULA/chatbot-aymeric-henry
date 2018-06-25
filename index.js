d19ef3a618a9bd3d8491da1eda63785c58b24673
henryJOKULA <henry@jokula.is>
2018-06-25 13:40:47 +0000

    new api key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const weatherApiKey = 'gIEGRkXIwctvG6vAZeuinDIi9JHz9w4w';
+const weatherApiKey = 'GWKKH6TUHPlDk5RDFb8W3aU2Y6h6n5Jt';
 
 app.use(bodyParser.json());
 
